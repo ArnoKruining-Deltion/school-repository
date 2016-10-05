bf145f1ae5b674cce2c05827bd80e310846c98be
ArnoKruining-Deltion <akruining@gmail.com>
2016-10-05 14:50:47 +0200

    alle unity project files added to the repository

---
+++ year 1/p1/test project/Assets/testing2.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testing2 : MonoBehaviour {
+
+
+    int teller = 0;
+    string klaar = "klaar";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(teller < 500)
+        {
+            teller++;
+            print(teller);  
+        }
+        else if (teller == 500)
+
+        {
+            teller++;
+            print(klaar);
+        
+        }
+        
+	}
+}

945c04b3500b7f72e16bf9e9a9c076e98e71839b
ArnoKruining-Deltion <akruining@gmail.com>
2016-11-04 10:17:31 +0100

    update pinball machine

---
+++ year 1/p1/New Unity Project/Assets/scripts/FlipperMovment.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipperMovment : MonoBehaviour {
+    public float flipperStrength;
+    public float pushForce;
+    private HingeJoint hinge;
+
+
+    void Start() {
+        hinge = GetComponent<HingeJoint>();
+    }
+
+
+    void Update() {
+
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector3 f = transform.up * flipperStrength;
+            Vector3 p = (transform.right) + transform.position * pushForce;
+            GetComponent<Rigidbody>().AddForceAtPosition(f, p);
+        }
+    }
+}
---
+++ year 1/p1/New Unity Project/Assets/scripts/TestScript3.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TestScript3 : MonoBehaviour {
+  
+    public bool x;
+    public bool y;
+    public bool z;
+    public bool rx;
+    public bool ry;
+    public bool rz;
+    public Vector3 test3;
+    public int score;
+    public Text scoreVeld;
+    public Rigidbody red;
+
+   
+
+
+    // Use this for initialization
+    void Start () {
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = test3;
+
+        if ( x == true)
+        {
+            test3.x += 0.1f;
+        }
+        if(y == true)
+        {
+            test3.y += 0.1f;
+        }
+        if(z == true)
+        {
+            test3.z += 0.1f;
+        }
+        if (rx == true)
+        {
+            test3.x -= 0.1f;
+        }
+        if (ry == true)
+        {
+            test3.y -= 0.1f;
+        }
+        if (rz == true)
+        {
+            test3.z -= 0.1f;
+        }
+        
+
+    }
+    void OnCollisionEnter()
+    {
+        score = score + 10;
+        scoreVeld.text = score.ToString();
+        red.AddForce(0, 0, 1000,ForceMode.Impulse);
+        
+
+    }
+}

a81f68d313aca9586ab0ad53deef321cea36f1fb
ArnoKruining-Deltion <akruining@gmail.com>
2016-11-07 11:01:56 +0100

    update

---
+++ P1/New Unity Project/Assets/scripts/ShootBall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShootBall : MonoBehaviour {
+
+   public Rigidbody rad;
+
+	
+    // Use this for initialization
+	void Start () {
+        rad.AddForce(0, 0, -10, ForceMode.Impulse);
+	}
+	
+	
+
+}
